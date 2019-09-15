e7b1a7e4b3c39ff41fd655584587c1f9dd7cc2e2
ThomasL <thomas.louis971@hotmail.com>
2019-09-15 10:16:10 -0400

    Default Changelist

--- src/Test/EnsembleChaineTest.java
+++ src/Test/EnsembleChaineTest.java
@@ -21,15 +21,40 @@ public class EnsembleChaineTest {
 
         String tab3[]={"a,b,c","c,d,e"};
         e1.ajouter(tab3);
-        assertTrue("si la liste contient un element particulie",e1.liste.contains("e"));
+        assertTrue("si la liste contient un element particulier",e1.liste.contains("e"));
 
         String tab4[]={"a,b,c","j,k,o","p,o,m"};
         e1.ajouter(tab4);
         assertTrue("si la liste contiient plusieurs elements",e1.liste.contains("e,h,y"));
 
 
-
     }
 
+    public boolean EnsembleChaine testUnion(){
+        EnsembleChaine e2 = new EnsembleChaine();
+        String tab1[] = {"a,z,e"};
+        e2.ajouter(tab1);
+        assertTrue("si la liste possède les elements suivants",e2.liste.contains(("a,z,f")));
+    }
+
+    public void EnsembleChaine testIntersection(){
+        EnsembleChaine e3 = new EnsembleChaine();
+        String tab2[] = {"a,t,y"};
+        e3.ajouter(tab2);
+        assertTrue("vérifer l'intersection", e3.liste.contains("z,f,g"));
+
+        String tab3[] = {"a,t,i"};
+        e3.ajouter(tab2);
+        assertTrue("il n'y a pas d'intersection", e3.liste.contains("o,p,s"));
+
+        String tab4[] = {"a,q,w","z,s,x"};
+        e3.ajouter(tab4);
+        assertTrue("verifier les intersections dans deux tableau", e3.liste.contains("q,w,x"));
+
+
+
+
+
+    }
 
 }
